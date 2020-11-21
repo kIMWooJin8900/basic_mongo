cddc0d074d5350b36335a8483d44d2f96c905311
KWJ <dnwls8900@naver.com>
2020-11-21 14:06:52 +0900

    m

--- src/app.js
+++ src/app.js
@@ -10,47 +10,56 @@ import path from "path";
 const PORT = 7000;
 
 const app = express();
-app.use(morgan(`dev`))
+app.use(morgan(`dev`));
 app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname,"/assets")));
+app.use(express.static(path.join(__dirname, "/assets")));
 
-mongoose.connect(`mongodb://4leaf:fourleaf0309@192.168.219.115:27017/admin`, {
+mongoose.connect(
+  `mongodb://4leaf:fourleaf0309@192.168.219.149:27017/admin`,
+  {
     dbName: `EDU_1`,
-    useNewURlParser: true,
+    useNewUrlParser: true,
     useCreateIndex: true,
-},
- (error) => {
-     if (error) {
-      console.log("Failed To DB Connect");
-     } else {
-         console.log("CUNNECT TO DB!")
-     }
- })
+  },
+  (error) => {
+    if (error) {
+      console.log("❌Failed To DB Connect");
+    } else {
+      console.log("✅ CUNNECT TO DB! ✅");
+    }
+  }
+);
+app.get("/", async (req, res) => {
+  console.log("🐶CALLED BY USER");
 
-app.get("/", async(req, res) => {
-    console.log("CALLED EY USER!")
+  const result = await Lecture.find({}, {});
 
-    const result = await Lecture.find({},{});
-    
-    return res.render("home", {LectureList: result});
+  return res.render("home", { lectureList: result });
 });
-
-app.get("/lecture", async(req, res) => {
-
-    const result = await Lecture.find({},{});
-
-    res.render("lecture", { dataList: result});
-});
-
 
 app.get("/snack", async (req, res) => {
-    const result = await Snack.find({},{});
+  const result = await Snack.find({}, {});
 
-    console.log(result, { dataList: result});
-})
+  console.log(result);
+});
+app.get("/lecture", async (req, res) => {
+  const result = await Lecture.find({}, {});
+  res.render("lecture", { dataList: result });
+});
 
+app.get("/board", (req, res) => {
+  res.render("board");
+});
 
-
+app.get("/test", (req, res) => {
+  res.render("layouts/main");
+});
+app.get("/test1", (req, res) => {
+  res.render("screens/test1");
+});
+app.get("/test2", (req, res) => {
+  res.render("screens/test2");
+});
 app.listen(PORT, () => {
-    console.log(`${PORT} SERVER STSRT`)
-})
+  console.log(`${PORT} server start`);
+});
